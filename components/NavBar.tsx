f35cf8b2d4b4c8c1ef4fb966afbe41d44394c13a
franc_mauric1 <franc_mauric1@hotmail.com>
2023-06-15 22:30:26 +0200

    provando estilos del hover

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -2,11 +2,11 @@ export default function NavBar () {
 
     return(
         <>
-            <div className="flex flex-wrap text-white text-2xl justify-between w-full h-30 bg-gray-500" >
-                <h2 className="p-3">Home</h2>
-                <h2 className="p-3">Servicios</h2>
-                <h2 className="p-3">About</h2>
-                <h2 className="p-3">Turnos Online</h2>
+            <div className="flex flex-wrap text-white text-2xl opacity-70 hover:opacity-100 justify-between w-full h-30 bg-gray-500" >
+                <h2 className="p-3 cursor-pointer">Home</h2>
+                <h2 className="p-3 cursor-pointer" >Servicios</h2>
+                <h2 className="p-3 cursor-pointer">About</h2>
+                <h2 className="p-3 cursor-pointer  hover:shadow-indigo-500">Turnos Online</h2>
             </div>
         </>
     )

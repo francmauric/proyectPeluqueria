3115b26f81fa8bd86f080fba629acfb2498f192d
franc_mauric1 <franc_mauric1@hotmail.com>
2023-06-21 15:37:13 +0200

    barra lateral

--- components/NavBarmobile.tsx
+++ components/NavBarmobile.tsx
@@ -14,14 +14,18 @@ export default function NavBar () {
                     aria-hidden="true"
                 />
             </Disclosure.Button>
-         
+            <div className="p-6 w-1/2 h-screen bg-white z-20 fixed top-0 -left-96 lg:left-0 lg:w-60  peer-focus:left-0 peer:transition ease-out delay-150 duration-200">
+              <div className="flex flex-col justify-start item-center">
+
+                 <div className="flex flex-wrapbg-gray-500 hidden" >
+                     <h2 className="p-3 cursor-pointer hover:bg-gray-500 rounded h-14">Home</h2>
+                     <h2 className="p-3 cursor-pointer hover:bg-gray-500 rounded h-14" >Servicios</h2>
+                     <h2 className="p-3 cursor-pointer hover:bg-gray-500 rounded h-14">Nuestro Salon</h2>
+                     <button className="bg-indigo-500 h-16 ring-white-800 hover:ring-2 opacity-70 hover:opacity-100  p-3 cursor-pointer px-3 rounded "><h2 className="">Turnos Online</h2></button>
+                 </div>
+              </div>
+            </div>     
          </Disclosure>
-            <div className="flex flex-wrapbg-gray-500 hidden" >
-                <h2 className="p-3 cursor-pointer hover:bg-gray-500 rounded h-14">Home</h2>
-                <h2 className="p-3 cursor-pointer hover:bg-gray-500 rounded h-14" >Servicios</h2>
-                <h2 className="p-3 cursor-pointer hover:bg-gray-500 rounded h-14">Nuestro Salon</h2>
-                <button className="bg-indigo-500 h-16 ring-white-800 hover:ring-2 opacity-70 hover:opacity-100  p-3 cursor-pointer px-3 rounded "><h2 className="">Turnos Online</h2></button>
-            </div>
         </>
     )
 }

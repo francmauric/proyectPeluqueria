b3c0e5c8c726d33b57a9eff0f6af91705f6f667e
franc_mauric1 <franc_mauric1@hotmail.com>
2023-07-01 13:22:26 +0200

    carousel

--- components/Carrousel.tsx
+++ components/Carrousel.tsx
@@ -1,8 +1,19 @@
+import { ChevronLeft, ChevronRight } from "react-feather"
+
 export default function Carrousel ({ children: slides }) {
 
+
     return(
-        <div>
-            <div>{slides}</div>
+        <div className="overflow-hidden relative">
+            <div className="flex">{slides}</div>
+            <div className="absolute inset-0 flex items-center justify-between p-4">
+                <button className="p-1 rounded-full shadow bg-white/80 text-gray-800 hover:bg-white">
+                    <ChevronLeft size={40}/>
+                </button>
+                <button className="p-1 rounded-full shadow bg-white/80 text-gray-800 hover:bg-white">
+                    <ChevronRight size={40} />
+                </button>
+            </div>
         </div>
     )
 }

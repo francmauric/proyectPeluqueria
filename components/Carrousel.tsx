2bfef046bda9c7d93c739b2ce6924cf5fc83ce8b
franc_mauric1 <franc_mauric1@hotmail.com>
2023-07-04 11:56:06 +0200

    seccion nuestro salon, armando

--- components/Carrousel.tsx
+++ components/Carrousel.tsx
@@ -46,11 +46,28 @@ export default function Carrousel ({ children: slides, autoslide= false, autosli
             </div>
         </div>
         <div className="flex-wrap lg:justify-around bg-gray-400 hidden sm:flex lg:flex-row sm:flex-col sm:content-center">
-            <div className="flex bg-gray-800">
-                        <h1>Nuestro Salon</h1>
-                        <p>Podras visualizar todos nuestros servicios </p>
+            <div className="flex bg-gray-800 flex-col content-center text-[#FFFFFF]" >
+                        <h1 className="text-center text-3xl p-5">Nuestro Salon</h1>
+                        <div className="p-4">
+                            <p >"En How You Want, nos enorgullece
+                             ofrecerte una experiencia de belleza excepcional
+                              en nuestro moderno y acogedor salón. Nuestro 
+                              objetivo es brindarte un servicio personalizado
+                               y de alta calidad que resalte tu belleza única.</p>
+                            <p> Sumérgete en nuestro elegante entorno mientras te deleitas con una taza de café recién hecho. Nuestro talentoso equipo de estilistas está aquí para escuchar tus necesidades y ayudarte a lograr el estilo de cabello perfecto que siempre has deseado. Con una combinación de habilidad artística y conocimientos actualizados sobre las últimas tendencias, te garantizamos resultados impresionantes.</p>
+
+                            <p> En nuestra galería de imágenes, podrás apreciar la belleza y el ambiente relajado de nuestro salón. Te invitamos a explorar nuestras instalaciones modernas, donde cada detalle ha sido cuidadosamente diseñado para brindarte una experiencia única.</p>
+
+                            <p> Nuestros servicios van más allá del corte y peinado. También ofrecemos una amplia gama de tratamientos capilares, desde coloraciones personalizadas y mechas de moda hasta alisados y estilizados profesionales. Además, contamos con servicios especializados para ocasiones especiales, como peinados y maquillajes para bodas o eventos importantes.</p>
+
+                            <p> En How You Want, la satisfacción del cliente es nuestra prioridad número uno. Estamos comprometidos a superar tus expectativas y garantizar que te sientas increíblemente hermoso/a en cada visita.</p>
+
+                            <p> Visítanos hoy mismo y déjanos consentirte con nuestros servicios excepcionales y nuestra atención dedicada. ¡Confía en nosotros para realzar tu belleza y elevar tu confianza!"</p>
+
+                            <p> Recuerda adaptar el texto según las características específicas de tu peluquería y los servicios que ofrezcas. ¡Espero que este ejemplo sea útil para tu sección de imágenes y servicios! </p>
+                        </div>        
             </div>
-            <div className="overflow-hidden relative h-full  max-w-md sm:rounded-br-full rounded-tl-full shadow-[-30px_-40px_60px_-5px_rgba(0,0,0,0.3)]" 
+            <div className="overflow-hidden relative h-full  max-w-md rounded-tl-full shadow-[-30px_-40px_60px_-5px_rgba(0,0,0,0.3)]" 
                 
                 >
                <div className="flex transition-transform ease-out duration-500"
--- components/Main.tsx
+++ components/Main.tsx
@@ -1,27 +1,32 @@
-import React from "react"
-import Carrousel from './Carrousel'
+import React from "react";
+import Carrousel from "./Carrousel";
 
-export default function Main () {
+export default function Main() {
+  const slides = [
+    "../images/salon-1.jpg",
+    "../images/salon-2.jpg",
+    "../images/salon-3.jpg",
+    "../images/salon-4.jpg",
+    "../images/salon-5.jpg",
+    "../images/salon-6.jpg",
+  ];
 
-    const slides = [
-        "../images/salon-1.jpg",
-        "../images/salon-2.jpg",
-        "../images/salon-3.jpg",
-        "../images/salon-4.jpg",
-        "../images/salon-5.jpg",
-        "../images/salon-6.jpg",
-    ]
-
-
-    return(
-      <div>
-        <div className="bg-[#5D4037] flex flex-col sm:flex-row sm:items-center sm:justify-center   ">
-            <div className="">
-                <div className="flex justify-center w-full text-4xl p-10 text-[#FFFFFF] ">La mejor peluqueria de Santa Croce Camerina</div>
-                <p className="text-white text-lg p-10">"¡Bienvenidos a HOW YOU WANT! Somos un equipo apasionado de estilistas y 
-                    especialistas en cuidado capilar dedicados a realzar tu belleza y brindarte una experiencia única. Nuestro objetivo es no solo transformar tu apariencia, sino también elevar tu confianza y bienestar personal.</p>
-            </div>
-            {/* <div className="max-w-screen-md px-full py-4 mx-auto">
+  return (
+    <div>
+      <div className="bg-[#5D4037] flex flex-col sm:flex-row sm:items-center sm:justify-center   ">
+        <div className="">
+          <div className="flex justify-center w-full text-4xl p-10 text-[#FFFFFF] ">
+            La mejor peluqueria de Santa Croce Camerina
+          </div>
+          <p className="text-white text-lg p-10">
+            "¡Bienvenidos a HOW YOU WANT! Somos un equipo apasionado de
+            estilistas y especialistas en cuidado capilar dedicados a realzar tu
+            belleza y brindarte una experiencia única. Nuestro objetivo es no
+            solo transformar tu apariencia, sino también elevar tu confianza y
+            bienestar personal.
+          </p>
+        </div>
+        {/* <div className="max-w-screen-md px-full py-4 mx-auto">
                <div className="bg-gray-400 flex justify-center mb-12  w-2/5 m-black">
                 <figure className="relative isolate flex flex-1 h-96 bg-black   overflow-hidden transition duration-500 hover:flex-5  opacity-75 hover:opacity-100">
                 <img className="w-full h-full absolute top-0 right-0 -z-1 object-cover " src="images/rulos.jpg" alt="rulos" />
@@ -33,44 +38,38 @@ export default function Main () {
                 <img className="w-60 h-96 absolute top-0 right-0 -z-1 object-cover " src="images/rubia.jpg" alt="rubio" />
                 </figure>
                </div>
-            </div> */}    
-            <div className="container">
-                <div className="gallery-wrapper">
-                    <figure className="gallery-item">
-                        <img className="item-image" src="/images/rulos.jpg" alt="rulos" />
-                        <figcaption className="item-description">
-                            <h2 className="name">Tus mejores Rulos</h2>
-                        </figcaption>
-                    </figure>
-                    <figure className="gallery-item">
-                        <img className="item-image" src="/images/rubia.jpg" alt="rubia" />
-                        <figcaption className="item-description">
-                            <h2 className="name">Tu mejor brillo</h2>
-                        </figcaption>
-                    </figure>
-                    <figure className="gallery-item">
-                        <img className="item-image" src="/images/lazio.jpg" alt="lazio" />
-                        <figcaption className="item-description">
-                            <h2 className="name">TU cabello Lazio</h2>
-                        </figcaption>
-                    </figure>
-                </div>
-            </div>    
+            </div> */}
+        <div className="container">
+          <div className="gallery-wrapper">
+            <figure className="gallery-item">
+              <img className="item-image" src="/images/rulos.jpg" alt="rulos" />
+              <figcaption className="item-description">
+                <h2 className="name">Tus mejores Rulos</h2>
+              </figcaption>
+            </figure>
+            <figure className="gallery-item">
+              <img className="item-image" src="/images/rubia.jpg" alt="rubia" />
+              <figcaption className="item-description">
+                <h2 className="name">Tu mejor brillo</h2>
+              </figcaption>
+            </figure>
+            <figure className="gallery-item">
+              <img className="item-image" src="/images/lazio.jpg" alt="lazio" />
+              <figcaption className="item-description">
+                <h2 className="name">TU cabello Lazio</h2>
+              </figcaption>
+            </figure>
+          </div>
         </div>
-        
-        <div className=" flex flex-col items-center">
-            <h2 className="bg-[#5D4037] text-3xl h-96 w-full text-[#FFFFFF] text-center">Nuestros Servicios</h2>
-            <div className="bg-gray-400 h-96 w-80">
-                Nuestros clientes nos escogen desde hace mucho tiempo y nos valoran como una de las peluquerías referentes de Barcelona. Profesionalismo, formación, compromiso y pasión por nuestro trabajo es lo que nos define. ¡Dinos lo que deseas y te será concedido!
-            </div>
-        </div>
-
-        <Carrousel autoslide= {true} >
-                {[
-                    ...slides.map((s) => <img src={s} />), ""
-                ]}
-        </Carrousel>        
-
       </div>
-    )
+
+      <div className=" flex flex-col items-center">
+        <div>
+          <Carrousel autoslide={true}>
+            {[...slides.map((s) => <img src={s} />), ""]}
+          </Carrousel>
+        </div>
+      </div>
+    </div>
+  );
 }

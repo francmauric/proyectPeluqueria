dfe0fda5cdcec291c3b2053d285d3fee7444dcc5
franc_mauric1 <franc_mauric1@hotmail.com>
2023-07-10 12:31:46 +0200

    empezando a diseñar bien el footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,12 +2,19 @@ export default function Footer () {
 
     return(
         <>
-            <div className="flex flex-wrap text-white text-2xl opacity-70 hover:opacity-100  items-center justify-around w-full h-36 bg-gray-900" >
+            <div className="p-10 bg-gray-800 text-gray-200">
+                <div className="max-w-7xl mx-auto">
+                    <div className="flex flex-1 md:flex-2 lg:flex-4 gap-2">
+
+                    </div>   
+                </div>
+            </div>
+            {/* <div className="flex flex-wrap text-white text-2xl opacity-70 hover:opacity-100  items-center justify-around w-full h-36 bg-gray-900" >
                 <h2 className="p-3 cursor-pointer hover:text-indigo-600">Linkedin</h2>
                 <h2 className="p-3 cursor-pointer hover:text-indigo-600" >Instagram</h2>
                 <h2 className="p-3 cursor-pointer hover:text-indigo-600">Facebook</h2>
                 <h2 className="p-3 cursor-pointer hover:text-indigo-600">Twitter</h2>
-            </div>
+            </div> */}
         </>
     )
 }

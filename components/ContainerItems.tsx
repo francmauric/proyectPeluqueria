678b2149ab1f78b8810797a60b85c5e58acbe977
franc_mauric1 <franc_mauric1@hotmail.com>
2023-07-11 12:04:21 +0200

    trabajando en el footer

--- components/ContainerItems.tsx
+++ components/ContainerItems.tsx
@@ -1,8 +1,14 @@
+import Item from "./Item";
+import { PRODUCTS, RESOURCES, COMPANY, SUPPORT } from "./Menu"
+
 export default function ContainerItems () {
 
     return (
-        <div>
-            <h1>items</h1>
+        <div className="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-4 gap-6 sm:px-8 px-5 py-16">
+            <Item Links={PRODUCTS} title="PRODUCTS"/>
+            <Item Links={RESOURCES} title="RESOURCES"/>
+            <Item Links={COMPANY} title="COMPANY"/>
+            <Item Links={SUPPORT} title="SUPPORT"/>
         </div>
     )
 }

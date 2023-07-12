a82508577360874abc35221a11feaaecc1aa79dd
franc_mauric1 <franc_mauric1@hotmail.com>
2023-07-12 12:11:15 +0200

    terminado el footer

--- components/SocialIcons.tsx
+++ components/SocialIcons.tsx
@@ -1,36 +1,60 @@
-/* import React from "react";
+import React from "react";
 import { Icons } from "./Menu"
 import { IonIcon } from "@ionic/react";
+/* import { logoFacebook } from "ionicons/icons"; */
+/* import { IonIcon } from "@ionic/react";*/
 import { logoFacebook, logoTwitter, logoGithub, logoLinkedin, logoInstagram } from "ionicons/icons";
 
-const iconMappings = {
-    "logo-facebook": logoFacebook,
-    "logo-twitter": logoTwitter,
-    "logo-github": logoGithub,
-    "logo-linkedin": logoLinkedin,
-    "logo-instagram": logoInstagram,
-  };
+const iconMappings = [
+    {logoFacebook},
+    {logoTwitter},
+    {logoGithub},
+    {logoLinkedin},
+    {logoInstagram},
+] 
 
 const SocialIcons = () => {
     return (
-        <div className="text-teal-500">
-            {Icons.map((icon) => (
-                <span
-                key={icon.name}
-                className="p-2 cursor-pointer inline-flex items-center
-                    rounded-full bg-gray-700 mx-1.5 text-xl hover:text-gray-100 hover:bg-teal-500 duration-300"
-                >
-                     <IonIcon icon={iconMappings[icon.name]} />
-            </span>
+        <div className="text-teal-400">
+           
             
-            ))}
+                <div
+                className="p-2 cursor-pointer inline-flex items-center
+                    rounded-full bg-gray-700 mx-1.5 text-xl hover:text-white hover:bg-teal-500 duration-300"
+                >
+                     <IonIcon icon={logoFacebook} />
+                </div>
+                <div
+                className="p-2 cursor-pointer inline-flex items-center
+                    rounded-full bg-gray-700 mx-1.5 text-xl hover:text-white hover:bg-teal-500 duration-300"
+                >
+                     <IonIcon icon={logoTwitter} />
+                </div>
+                <div
+                className="p-2 cursor-pointer inline-flex items-center
+                    rounded-full bg-gray-700 mx-1.5 text-xl hover:text-white hover:bg-teal-500 duration-300"
+                >
+                     <IonIcon icon={logoGithub} />
+                </div> 
+                <div
+                className="p-2 cursor-pointer inline-flex items-center
+                    rounded-full bg-gray-700 mx-1.5 text-xl hover:text-white hover:bg-teal-500 duration-300"
+                >
+                     <IonIcon icon={logoLinkedin} />
+                </div> 
+                <div
+                className="p-2 cursor-pointer inline-flex items-center
+                    rounded-full bg-gray-700 mx-1.5 text-xl hover:text-white hover:bg-teal-500 duration-300"
+                >
+                     <IonIcon icon={logoInstagram} />
+                </div>
         </div>
     )
 }
 
-export default SocialIcons; */
+export default SocialIcons;
 
-import React from "react";
+/* import React from "react";
 import { Icons } from "./Menu";
 import { IonIcon } from "@ionic/react";
 import { logoFacebook, logoTwitter, logoGithub, logoLinkedin, logoInstagram } from "ionicons/icons";
@@ -59,4 +83,4 @@ const SocialIcons = () => {
   );
 };
 
-export default SocialIcons;
+export default SocialIcons; */

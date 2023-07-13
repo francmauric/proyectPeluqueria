3383e6acd1c8234d7f9743d9f0acb1c801865704
franc_mauric1 <franc_mauric1@hotmail.com>
2023-07-13 17:44:21 +0200

    comenzando con logica para pedir turno, empezando con ventana emergente del button pedir turno

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,4 +1,17 @@
+import Modal from "react-modal";
+import React, {useState} from "react";
+
 export default function NavBar () {
+    const [isOpen, setIsOpen] = useState(false)
+
+    const handleOpenModal = () => {
+        setIsOpen(true);
+    }
+
+    const handleCloseModal = () => {
+        setIsOpen(false)
+    }
+
 
     return(
         <>
@@ -6,8 +19,13 @@ export default function NavBar () {
                 <h2 className="p-3 cursor-pointer hover:bg-gray-500 rounded h-14">Home</h2>
                 <h2 className="p-3 cursor-pointer hover:bg-gray-500 rounded h-14" >Servicios</h2>
                 <h2 className="p-3 cursor-pointer hover:bg-gray-500 rounded h-14">Nuestro Salon</h2>
-                <button className="bg-indigo-500  h-16 ring-white-800 hover:ring-2 opacity-70 hover:opacity-100  p-3 cursor-pointer px-3 rounded "><h2 className="">Turnos Online</h2></button>
+                <button onClick={handleOpenModal} className="bg-indigo-500  h-16 ring-white-800 hover:ring-2 opacity-70 hover:opacity-100  p-3 cursor-pointer px-3 rounded ">
+                    <h2 className="">Turnos Online</h2></button>
             </div>
+            <Modal isOpen={isOpen} onRequestClose={handleCloseModal}>
+                <h2>Formulario de reserva</h2>
+                <button onClick={handleCloseModal} >Cerrar</button>
+            </Modal>
         </>
     )
 }

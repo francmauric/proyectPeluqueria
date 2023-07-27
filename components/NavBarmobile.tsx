57bce396725f12e5eb9776c651eb68984e7cc39b
franc_mauric1 <franc_mauric1@hotmail.com>
2023-07-27 10:38:10 +0200

    cambio de icono en el navbarmobile y agregado de iframe en el componente direccion

--- components/NavBarmobile.tsx
+++ components/NavBarmobile.tsx
@@ -21,15 +21,15 @@ export default function NavBar () {
             <div className="p-6 w-1/2 h-screen bg-gradient-to-b from-orange-500 to-gray-400 z-100 bg-opacity-90 fixed top-0 -left-96 lg:left-0 lg:w-60  peer-focus:left-0 peer:transition ease-out delay-150 duration-200 sm:hidden">
               <div className="flex flex-col rounded justify-start item-center ">
                 <div className="flex rounded-b-3xl items-center justify-center bg-gray-500 bg-opacity-80 p-5 h-48 pb-10">
-                    <img className="rounded-full h-32 w-32" src="images/hair-salon.jpg" alt="hair" />
+                    <img className="rounded-full h-28 w-28" src="images/logo-HYW.png" alt="hair" />
                 </div>
                  <div className="flex rounded-t-3xl flex-col bg-opacity-80 justify-items-center bg-gray-500 " >
                      <h2 className="p-3 cursor-pointer hover:bg-gray-500 rounded h-14 text-center ">Home</h2>
-                     <h2 className="p-3 cursor-pointer hover:bg-gray-500 rounded h-14 text-center" >Servicios</h2>
-                     <h2 className="p-3 cursor-pointer hover:bg-gray-500 rounded h-14 text-center">Nuestro Salon</h2>
+                     <a href="#service" className="p-3 cursor-pointer hover:bg-gray-500 rounded h-14 text-center" >Servicios</a>
+                     <a href="#salon" className="p-3 cursor-pointer hover:bg-gray-500 rounded h-14 text-center">Nuestro Salon</a>
                      <button className="bg-indigo-500 h-16 ring-white-800 hover:ring-2 opacity-70 hover:opacity-100  p-3 cursor-pointer px-3 rounded text-center "><a href="#turnos" className="">Turnos Online</a></button>
                  </div>
-                 <div className="flex py-8 flex-col items-center justify-center">
+                 <div className="flex py-8 pt-16 text-white flex-col items-center justify-center">
                     <h2>Horario</h2>
                     <p>Lunes a sabado</p>
                     <p>8:00 AM  -  20:00 PM</p>

84eaa21e9504ae4e3dc58477c52a55f095724a22
franc_mauric1 <franc_mauric1@hotmail.com>
2023-07-31 17:50:51 +0200

    empezando a armar la logica para enviar por correo la informacion del formulario

---
+++ components/SendEmail.tsx
@@ -0,0 +1,14 @@
+const nodemailer = require("nodemailer");
+
+
+
+export default function SendEmail () {
+
+
+    
+    return(
+        <div>
+            <h1>sendemail</h1>
+        </div>
+    )
+}

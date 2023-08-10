2f84bef2ff16b188ed2cb5cd0793d31faf42b1ae
franc_mauric1 <franc_mauric1@hotmail.com>
2023-08-10 16:42:10 +0200

    intentando utilizar nodemailer

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -281,7 +281,7 @@ export default function NavBar () {
             </div>
 
             </Modal>
-            <SendEmail formData={formData} />
+            
            
             
             </div>
--- pages/api/hello.tsx
+++ pages/api/hello.tsx
@@ -1,20 +1,50 @@
 const { log } = console;
+import nodemailer from "nodemailer";
 
-export default function Hello (req, res) {
+
+export default async function Hello (req, res) {
 
 if (req.method === "POST") {
 
-    const data = req?.body;
+    const data = req.body;
     
     log("req payload:", data)
-    
-    res.json({data} );
 
-}
+    try{
+        const transporter = nodemailer.createTransport({
+            service: "gmail",
+            auth: {
+                user: process.env.EMAIL_USER,
+                pass: process.env.EMAIL_PASS,
+            },
+        });
+
+        const mailOptions = {
+            from: process.env.EMAIL_USER,
+            to: ["francocuello10@gmail.com"],
+            subject: "Nuevo formulario enviado",
+            text: JSON.stringify(data, null, 2),
+
+        };
+
+        await transporter.sendMail(mailOptions)
+
+        res.status(200).json({ message: "Correo enviado con éxito"})
+    }catch (error) {
+        console.error("Error al enviar el correo: ", error);
+        res.status(500).json({ error: "Ocurrio un error al enviar el correo"});
+    }
+}  else {
+    res.status(500).json({ error: "Esta debe ser ua solicitud POST"})
+} 
+
+
+
+/*     res.json({data} );
 
 return res.status(500).json({
     msg: "this needs to be a post request"
 })
 
-
+ */
 }
